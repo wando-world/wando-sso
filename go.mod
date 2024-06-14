dcdb5f4f889ac1d258668e4e61b1590d2173aac0
kdw1521 <kdw40161217@gmail.com>
2024-06-14 14:26:44 +0900

    test: 회원가입 db layer
    - sqlite driver 추가
    
    #3

--- go.mod
+++ go.mod
@@ -29,6 +29,7 @@ require (
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
@@ -40,4 +41,5 @@ require (
 	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/sqlite v1.5.6 // indirect
 )
---
+++ internal/db/user_db_test.go
@@ -0,0 +1,53 @@
+package db
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/wando-world/wando-sso/internal/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"testing"
+)
+
+func setupUserTestDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{DisableForeignKeyConstraintWhenMigrating: true})
+
+	if err != nil {
+		panic("sqlite 커넥션 실패")
+	}
+	err = db.AutoMigrate(&models.User{})
+	if err != nil {
+		panic("마이그레이션 실패")
+	}
+	return db
+}
+
+func TestCreateUser(t *testing.T) {
+	db := setupUserTestDB()
+	repo := NewUserRepository(db)
+
+	email := "wando@naver.com"
+	testUser := &models.User{
+		// 모델에 맞는 필드값 설정
+		Nickname:     "wando",
+		UserID:       "wando123",
+		Password:     "password123",
+		Email:        &email,
+		Salt:         "123",
+		VerifiedCode: "123456",
+		Role:         "GENERAL",
+	}
+
+	// CreateUser를 호출하고 결과를 검사
+	err := repo.CreateUser(testUser)
+	assert.NoError(t, err, "회원가입 성공")
+
+	// 데이터가 실제로 삽입되었는지 검증
+	var user models.User
+	result := db.First(&user, "nickname = ?", "wando")
+	assert.NoError(t, result.Error, "회원가입 후 조회 성공")
+	assert.Equal(t, "wando@naver.com", *user.Email, "이메일 맞는지 확인")
+
+	// 이미 있는 유저 생성시 에러발생
+	err = repo.CreateUser(testUser)
+	assert.Error(t, err, "이미 있는 유저이므로 에러 발생")
+}
